c928df03242e99c62b6afbbfa5d46004f479d12d
shafeeque <shafeeqsha06@gmail.com>
2025-10-01 22:19:28 +0530

    feat(user): add user orm and user profile orm entiry

---
+++ src/infrastructure/database/entities/user-profile-orm.entiry.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, OneToOne, PrimaryColumn, CreateDateColumn, JoinColumn } from "typeorm";
+import { UserOrmEntity } from "./user-orm-entity";
+
+@Entity("user_profiles")
+export class UserProfileOrmEntity {
+  @PrimaryColumn("uuid")
+  id!: string;
+  
+  @Column({ nullable: false })
+  userId: string;
+  
+
+  @OneToOne(() => UserOrmEntity, (u) => u.profile)
+  @JoinColumn({ name: "userId" })
+  user!: UserOrmEntity;
+
+  @Column({ type: "text", nullable: true })
+  bio?: string;
+
+  @Column({ nullable: true })
+  phone?: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  country?: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  city?: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  language?: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  website?: string;  
+
+  @Column({ type: "enum", enum: ["male", "female", "other"], nullable: true })
+  gender?: "male" | "female" | "other";
+
+  @Column({ type: "jsonb", nullable: true })
+  preferences?: Record<string, any>;
+
+  @CreateDateColumn()
+  createdAt: Date
+
+
+}
