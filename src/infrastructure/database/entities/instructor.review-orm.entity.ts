0afc9fc70b60867e320b416af799cd5581717d37
shafeeque <shafeeqsha06@gmail.com>
2025-10-01 22:17:42 +0530

    feat(instructor): add instructor profile and instructorReview entities

---
+++ src/infrastructure/database/entities/instructor.review-orm.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { InstructorProfileOrmEntity } from "./instructor-profile-orm.entity";
+import { UserOrmEntity } from "./user-orm-entity";
+
+@Entity("instructor_reviews")
+export class InstructorReviewOrmEntity {
+  @PrimaryColumn("uuid")
+  id!: string;
+
+  @ManyToOne(() => InstructorProfileOrmEntity, (i) => i.reviews, {
+    onDelete: "CASCADE",
+  })
+  instructor!: InstructorProfileOrmEntity;
+
+  @ManyToOne(() => UserOrmEntity, { onDelete: "SET NULL", nullable: true })
+  student?: UserOrmEntity;
+
+  @Column({ type: "int" })
+  rating!: number;
+
+  @Column({ type: "text", nullable: true })
+  reviewText?: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
